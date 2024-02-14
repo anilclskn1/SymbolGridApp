874ba311cdb87932bb721855e4ed683aa5f80876
Anil Caliskan <anilclskn.1@gmail.com>
2024-02-14 13:20:14 +0300

    Initial Commit

---
+++ SymbolGridApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SymbolGridApp
+//
+//  Created by Anil Caliskan on 14.02.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SymbolGridApp/SymbolGridAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SymbolGridAppApp.swift
+//  SymbolGridApp
+//
+//  Created by Anil Caliskan on 14.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct SymbolGridAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
